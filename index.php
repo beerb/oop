133e5b27e6cac8d2c10d26f1a74d9b06afd6e640
beerb <beerbutle@yandex.ru>
2016-04-23 18:06:17 +0300

    new

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: slay
- * Date: 23.04.2016
- * Time: 14:12
- */ echo "www";
+session_start();
+header("Content-Type: text/html; charset=utf-8");
+?>
+Привет!
